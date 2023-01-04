e64ea14e91a727ebac65abbf0bbf80083a8850a6
Enei <eneipereira@outlook.com.br>
2023-01-04 19:24:32 -0300

    feat: Cria Dockerfile para o backend e inclui no docker-compose

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -2,12 +2,13 @@ const { env } = process;
 
 const config = {
   username: env.MYSQL_USER || 'root',
-  password: env.MYSQL_PASSWORD || 'root',
+  password: env.MYSQL_PASSWORD || '123456',
   database: env.MYSQL_DATABASE || 'cashforce_v3',
   host: env.MYSQL_HOST || 'localhost',
   port: env.MYSQL_PORT || '3306',
   dialect: 'mysql',
   dialectOptions: env.MYSQL_OPTIONS ? JSON.parse(env.MYSQL_OPTIONS) : {},
+  logging: false,
 };
 
 module.exports = {
